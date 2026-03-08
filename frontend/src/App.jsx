ea3c65c11e63ba9a6c78edea33f1f124769e6cc0
ishanrt119 <ishan.toraskar23@pccoepune.org>
2026-03-08 11:48:40 +0530

    update homa and app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/Footer.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
+import RepoDashboard from './pages/RepoDashboard.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#0a0a0a]">
-      <Navbar />
-      <Home />
-      <Footer />
-    </div>
+    <Router>
+      <div className="min-h-screen bg-[#0a0a0a]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/repo" element={<RepoDashboard />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
